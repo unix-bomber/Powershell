5189bb2d1e3d8c5bee08a05c40bc28a4cca024d5
unix-bomber <timothykaczynski@gmail.com>
2019-10-03 11:25:00 -0400

    Initial commit
    Added branch, added SlefSigned HTTPS GET

--- decodebase64.ps1
+++
@@ -1,8 +0,0 @@
-$file = "pathtofile"
-#change delimiter based on requirements
-$data = Get-Content $file -Delimiter '|'
-#remove delimiter
-$data = $data.replace("|","")
-foreach ($d in $data){
-  [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String("$d")) | Out-File -Encoding "ASCII" "pathtodestinationfile" -append
-  }
