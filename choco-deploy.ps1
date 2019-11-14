fba099fb638836d8fc40dc0e2e83e4d241dfb40c
unix-bomber <timothykaczynski@gmail.com>
2019-11-13 20:26:17 -0500

    Removed formatting issues

--- choco-deploy.ps1
+++ choco-deploy.ps1
@@ -1,4 +1,21 @@
-$wsusserver = $env:Computername$adserver = $env:LOGONSERVER.substring(2)$ServiceAccount = "domain\some-svc-acct" #service account needs to be in remote group, access group$Password = ConvertTo-SecureString -string "sanitizedpassword" -AsPlainText -force$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ServiceAccount, $Password#Import AD Module$S = New-PSSession -ComputerName $adserver -Credential $Credential$ComputerProperties = Invoke-Command $S -Scriptblock { Get-ADGroupmember -Identity "Domain Computers" }#Get All Computers to update$ComputerName = $ComputerProperties.Name    foreach ($Computer in $Computername)         {                Invoke-Command -Computername $computer -Credential $Credential -Scriptblock {
+$wsusserver = $env:Computername
+$adserver = $env:LOGONSERVER.substring(2)
+
+$ServiceAccount = "domain\some-svc-acct" #service account needs to be domain admin
+$Password = ConvertTo-SecureString -string "sanitizedpassword" -AsPlainText -force
+
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ServiceAccount, $Password
+
+#Import AD Module
+$S = New-PSSession -ComputerName $adserver -Credential $Credential
+$ComputerProperties = Invoke-Command $S -Scriptblock { Get-ADGroupmember -Identity "Domain Computers" }
+#Get All Computers to update
+$ComputerName = $ComputerProperties.Name
+
+    foreach ($Computer in $Computername) 
+        {
+                Invoke-Command -Computername $computer -Credential $Credential -Scriptblock {
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco feature enable -n usefipscompliantchecksums
